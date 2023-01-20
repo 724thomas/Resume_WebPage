4717d0870dce1d8fac5be49e20ad7b49a8e0c3ff
WonJoonChoi <724thomas@gmail.com>
2023-01-20 16:00:43 +0900

    Initial commit

---
+++ src/main/java/com/example/resume_webpage/ResumeWebPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.resume_webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeWebPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeWebPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resume_webpage/ResumeWebPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resume_webpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeWebPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
