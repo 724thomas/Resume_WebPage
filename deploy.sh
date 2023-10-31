daa1544103d2a4c08f7655d2dfe30654aa2c03f3
724thomas <724thomas@gmail.com>
2023-10-31 15:43:31 +0900

    포트폴리오 Refactor

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@
  JAR_PATH="/home/ubuntu/cicd/*.jar"
  echo "jar path : $JAR_PATH"
  chmod +x $JAR_PATH
- nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+ nohup java -jar $JAR_PATH &
  echo "jar fild deploy success"
